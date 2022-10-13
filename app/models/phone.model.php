e2a9af75c7099dc7a8e709b1e13559fc7817ffcf
Héctor Bruña <hectorbct.2002@gmail.com>
2022-10-12 23:54:13 -0300

    filtro de busqueda por marca

--- app/models/phone.model.php
+++ app/models/phone.model.php
@@ -18,25 +18,39 @@ class PhoneModel {
         $query = $this->db->prepare("SELECT phones.*, brands.brand_name as brand FROM phones JOIN brands ON phones.id_brand = brands.id_brand WHERE id = $id");
         $query->execute();
         $phone = $query->fetch(PDO::FETCH_OBJ);
-        
         return $phone;
     }
     
 
-    public function insertPhone($img, $model, $memory, $display,$cpugpu,$camera,$id_brand,) {
+    public function insertPhone($model, $memory, $display,$cpugpu,$camera,$id_brand, $img = null) {
+        $pathImg = null;
+        if ($img)
+            $pathImg = $this->uploadImage($img);
+
         $query = $this->db->prepare("INSERT INTO phones (img, model, memory, display, cpugpu, camera, id_brand) VALUES (?, ?, ?, ?, ?,?,?)");
-        $query->execute([$img, $model, $memory, $display, $cpugpu, $camera, $id_brand]);
+        $query->execute([$pathImg, $model, $memory, $display, $cpugpu, $camera, $id_brand]);
     }
 
-    public function updatePhone($id,$img, $model, $memory, $display,$cpugpu,$camera,$id_brand,) {
-        $query = $this->db->prepare("UPDATE phones  (img, model, memory, display, cpugpu, camera, id_brand) VALUES (?, ?, ?, ?, ?,?,?) WHERE id = $id");
-        $query->execute([$img, $model, $memory, $display, $cpugpu, $camera, $id_brand]);
+    private function uploadImage($img){
+        $target = 'img/' . uniqid() . '.jpg';
+        move_uploaded_file($img, $target);
+        return $target;
     }
 
-    public function insertBrand($brand) {
-        $query = $this->db->prepare("INSERT INTO brands (brand_name) VALUES (?)");
-        $query->execute([$brand]);
+    public function deleteImage($phone){
+        unlink($phone->img);
     }
+
+    public function updatePhone($id, $model, $memory, $display, $cpugpu, $camera, $id_brand, $img=null) {
+        $pathImg = null;
+        if ($img)
+            $pathImg = $this->uploadImage($img);
+
+        $query = $this->db->prepare("UPDATE phones  SET img=?, model=?, memory=?, display=?, cpugpu=?, camera=?, id_brand=?  WHERE id = ?");
+        $query->execute([$pathImg, $model, $memory, $display, $cpugpu, $camera, $id_brand,$id]);
+    }
+
+
     function deletePhone($id) {
         $query = $this->db->prepare('DELETE FROM phones WHERE phones . id = ?');
         $query->execute([$id]);
