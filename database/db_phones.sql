2eb760a2732078f9f547777ccf30806fa58a04af
Héctor Bruña <hectorbct.2002@gmail.com>
2022-10-17 16:30:50 -0300

    arregle la base de datos

--- database/db_phones.sql
+++ database/db_phones.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-10-2022 a las 17:24:15
+-- Tiempo de generación: 17-10-2022 a las 21:29:33
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -51,7 +51,7 @@ INSERT INTO `brands` (`id_brand`, `brand_name`) VALUES
 
 CREATE TABLE `phones` (
   `id` int(11) NOT NULL,
-  `img` varchar(50) NOT NULL,
+  `img` varchar(50) DEFAULT NULL,
   `model` varchar(100) NOT NULL,
   `memory` varchar(100) NOT NULL,
   `display` varchar(200) NOT NULL,
@@ -65,9 +65,11 @@ CREATE TABLE `phones` (
 --
 
 INSERT INTO `phones` (`id`, `img`, `model`, `memory`, `display`, `cpugpu`, `camera`, `id_brand`) VALUES
-(22, 'img/6347747d9191e.jpg', 'Moto edg 30', 'RAM 8 GB /Int. Memory	256 GB', '6.7 inches 1080 x 2400 pixels', 'Snapdragon 778/ Adreno 642L', '108 MP + 8 MP + 16 MP', 1),
+(22, 'img/634b66b5f0e63.jpg', 'Moto edg 30', 'RAM 8 GB /Int. Memory	256 GB', 'OLED de 6,5\" FullHD+ con HDR10+144 Hz de tasa de refresco', 'Snapdragon 778/ Adreno 642L', 'Principal de 50 Mpx (f/1,8)\r\nGran angular de 50 Mpx (f/2,2) y 114º\r\nProfundidad de 2 Mpx (f/2,4)', 1),
 (23, 'img/634773b0db000.jpg', 'IPhone 13pro max', '128/256/512 GB/1 TB', 'Pantalla Super Retina XDR con ProMotion Resolución de 2778 x 1284 pixeles a 458 ppi', 'Apple A15 Bionic', 'Telefoto: 12 MP f/2.8, 77mm, 3x óptico Ultrawide: 12 MP f/1.8, 6P, 120º Principal: 12 MP f/1.5, 1,9um', 2),
-(29, 'img/634773d171762.jpg', 'Samsung Galaxy A53', '6 GB RAM/ 128 o 256 GB Almacenamiento', 'Super AMOLED Full HD+ de 6,5 pulgadas, 120 Hz (el refresco no es adaptativo) y cobertura Corning Gorilla Glass 5', 'Exynos 1280 de 8 núcleos', 'Principal: sensor de 64 megapíxeles, óptica con valor de apertura f/1.8 y estabilización óptica Ultra gran angular: sensor de 12 megapíxeles y óptica con valor de apertura f/2.2 Cámara de profundidad:', 3);
+(29, 'img/634b5f746beb5.jpg', 'Samsung Galaxy A53', 'RAM 8 GB /Int. Memory	256 GB', 'Super AMOLED Full HD+ de 6,5 pulgadas, 120 Hz (el refresco no es adaptativo) y cobertura Corning Gorilla Glass 5', 'Exynos 1280 de 8 núcleos', 'Principal: sensor de 64 megapíxeles, óptica con valor de apertura f/1.8 y estabilización óptica Ultra gran angular: sensor de 12 megapíxeles y óptica con valor de apertura f/2.2 Cámara de profundidad', 3),
+(41, 'img/634b60ae662d6.jpg', '	 XIAOMI MI 11 ULTRA', 'RAM 8,12 GB LPDDR5 / ALMACENAMIENTO  128,256,512 GB UFS 3.1', '6,81” WQHD+, 120 Hz, 480 Hz táctil\r\n10-bit AMOLED E4, 515 ppp\r\n1.550 nits, HDR10+, MEMC\r\nGorilla Glass Victus\r\nPantalla AMOLED 1,1\" secundaria trasera', 'Snapdragon 888', '50 MP (1/1,12”)8P, OIS, f/1.95\r\nGran angular IMX586 48MP, 128º, f/2.2\r\nTelemacro IMX586 48MP, 5x óptico, 120x digital', 7),
+(42, 'img/634b614ab1995.jpg', 'HUAWEI MATE 40 PRO', 'RAM  8 GB / ALMACENAMIENTO 256 GB', '	\r\nOLED 6,76 pulgadas\r\nFHD+ 2.772 x 1.344 píxeles\r\nTasa de refresco 90Hz', 'CPU Kirin 9000 Octa-core / GPU 24-core Mali-G78', 'Sensor principal: 50MP Ultra Vision Camera, f/1.9\r\nUltra angular: 20MP Cine Camera. f/1.8\r\nTeleobjetivo: 12MP, f/3.4, OIS', 4);
 
 -- --------------------------------------------------------
 
@@ -119,19 +121,29 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `brands`
 --
 ALTER TABLE `brands`
-  MODIFY `id_brand` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_brand` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de la tabla `phones`
 --
 ALTER TABLE `phones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `phones`
+--
+ALTER TABLE `phones`
+  ADD CONSTRAINT `phones_ibfk_1` FOREIGN KEY (`id_brand`) REFERENCES `brands` (`id_brand`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
